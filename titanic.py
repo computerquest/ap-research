f62f9b9c80dac0960c3b4a1e8dbdffd8f68befba
computerquest <jaredstigter@gmail.com>
2019-02-18 23:04:03 -0500

    got titanic working changed the titanic dataset added the preliminary titanic results

--- titanic.py
+++ titanic.py
@@ -39,50 +39,58 @@ print('the final frame is', dataframe)
 # MODEL TRAINING
 kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
 
-
-def experiment(kfold, x, y, dim, activation, init):
+def experiment(dim, activation, init, epochs, file):
+    x = 0
     print(init, activation)
     cvscores = []
+    c = 0
     for train, test in kfold.split(dataframe, target):
-        # create model
-        model = Sequential()
-        model.add(Dense(dim[0], input_dim=12, kernel_initializer=init, activation=activation))
-        for c in range(1, len(dim) - 1):
-            model.add(Dense(dim[0], kernel_initializer=init, activation=activation))
-        model.add(Dense(dim[-1], kernel_initializer=init, activation=activation))
+        print('this is split', c)
+        c += 1
+        for z in range(0, 5):
+            # create model
+            model = Sequential()
+            model.add(Dense(dim[0], input_dim=12, kernel_initializer=init, activation=activation))
+            for c in range(1, len(dim) - 1):
+                model.add(Dense(dim[0], kernel_initializer=init, activation=activation))
+            model.add(Dense(dim[-1], kernel_initializer=init, activation=activation))
 
-        # Compile model
-        model.compile(loss='mean_squared_error',
-                      optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False),
-                      metrics=['accuracy'])
+            # Compile model
+            model.compile(loss='mean_squared_error',
+                          optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False),
+                          metrics=['accuracy'])
 
-        # Fit the model
-        model.fit(dataframe[train], target[train], epochs=3000, batch_size=128, verbose=0,
-                  callbacks=[EarlyStopping(monitor='loss',
-                                           min_delta=0,
-                                           patience=30,
-                                           verbose=0, mode='auto')])
+            # Fit the model
+            model.fit(dataframe[train], target[train], epochs=epochs, batch_size=128, verbose=0,
+                      callbacks=[EarlyStopping(monitor='loss',
+                                               min_delta=-.000001,
+                                               patience=30,
+                                               verbose=0, mode='auto')])
 
-        # evaluate the model
-        scores = model.evaluate(dataframe[test], target[test], verbose=0)
-        print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
-        cvscores.append(scores[1] * 100)
+            # evaluate the model
+            scores = model.evaluate(dataframe[test], target[test], verbose=0)
+            print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+            cvscores.append(scores[1] * 100)
+
+            model.save(file+'_split'+str(c)+'_'+str(z)+'.h5')
+            x += 1
     print("%.2f%% (+/- %.2f%%)" % (numpy.mean(cvscores), numpy.std(cvscores)))
 
+
 print('small')
-experiment(kfold, dataframe, target, [5, 1], 'sigmoid', 'random_normal')
-experiment(kfold, dataframe, target, [5, 1], 'relu', 'random_normal')
-experiment(kfold, dataframe, target, [5, 1], 'sigmoid', 'glorot_normal')
-experiment(kfold, dataframe, target, [5, 1], 'relu', 'he_normal')
+experiment([5, 1], 'sigmoid', 'random_normal', 3000, './titanic_results/small/rand_sig')
+experiment([5, 1], 'relu', 'random_normal', 3000, './titanic_results/small/rand_relu')
+experiment([5, 1], 'sigmoid', 'glorot_normal', 3000, './titanic_results/small/xavier')
+experiment([5, 1], 'relu', 'he_normal', 3000, './titanic_results/small/he')
 
 print('medium')
-experiment(kfold, dataframe, target, [10, 1], 'sigmoid', 'random_normal')
-experiment(kfold, dataframe, target, [10, 1], 'relu', 'random_normal')
-experiment(kfold, dataframe, target, [10, 1], 'sigmoid', 'glorot_normal')
-experiment(kfold, dataframe, target, [10, 1], 'relu', 'he_normal')
+experiment([10, 1], 'sigmoid', 'random_normal', 3000, './titanic_results/medium/rand_sig')
+experiment([10, 1], 'relu', 'random_normal', 3000, './titanic_results/medium/rand_relu')
+experiment([10, 1], 'sigmoid', 'glorot_normal', 3000, './titanic_results/medium/xavier')
+experiment([10, 1], 'relu', 'he_normal', 3000, './titanic_results/medium/he')
 
 print('large')
-experiment(kfold, dataframe, target, [10, 5, 1], 'sigmoid', 'random_normal')
-experiment(kfold, dataframe, target, [10, 5, 1], 'relu', 'random_normal')
-experiment(kfold, dataframe, target, [10, 5, 1], 'sigmoid', 'glorot_normal')
-experiment(kfold, dataframe, target, [10, 5, 1], 'relu', 'he_normal')
+experiment([10, 5, 1], 'sigmoid', 'random_normal', 6000, './titanic_results/large/rand_sig')
+experiment([10, 5, 1], 'relu', 'random_normal', 6000, './titanic_results/large/rand_relu')
+experiment([10, 5, 1], 'sigmoid', 'glorot_normal', 6000, './titanic_results/large/xavier')
+experiment([10, 5, 1], 'relu', 'he_normal', 6000, './titanic_results/large/he')
