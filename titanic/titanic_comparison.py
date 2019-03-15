6695bd01c9d083905bc6b7fbb5ce605328ce9d97
Jared Stigter <spamacct517@@gmail.com>
2019-03-14 20:50:24 -0400

    this is phase one of analysis

--- titanic/titanic_comparison.py
+++ titanic/titanic_comparison.py
@@ -1,11 +1,39 @@
 from keras.models import load_model
+import numpy as np
 
-model = load_model('D:/PycharmProjects/ap-research/titanic/titanic_results/small/rand_sig_split1_0.h5')
-print(model.get_weights(), len(model.layers))
-print(model.summary())
 
-print('other model //////////////////////////////////')
-model = load_model('D:/PycharmProjects/ap-research/titanic/weights/small/rand_sig_split1_0.h5')
+def weight_delta(size, main):
+    ans = []
+    for split in range(1,6):
+        total_weights = np.zeros(shape=(4,))
+        for x in range(0, 5):
+            modela = load_model(
+                '/home/jstigter/PycharmProjects/ap-research/titanic/titanic_results/' + size + '/' + main + '_split' + str(split) + '_' + str(
+                    x) + '.h5')
+            #print(modela.summary())
 
-print(model.get_weights(), len(model.layers))
-print(model.summary())
+            modelb = load_model(
+                '/home/jstigter/PycharmProjects/ap-research/titanic/weights/' + size + '/' + main + '_split' + str(split) + '_' + str(
+                    x) + '.h5')
+            #print(modelb.summary())
+
+            difs = np.subtract(modelb.get_weights(), modela.get_weights())
+            total_weights = np.add(total_weights, difs)
+
+        temp = np.divide(total_weights, 5)
+
+        for c in temp:
+            flat_list = []
+            for sublist in c.tolist():
+                if isinstance(sublist, (list,)):
+                    for item in sublist:
+                        flat_list.append(item)
+                else:
+                    flat_list.append(sublist)
+            print('adding', flat_list)
+            ans.extend(flat_list)
+
+
+    return ans
+
+print(weight_delta('small', 'he'))
